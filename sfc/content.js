adac5a4892023d5bf4fc917bec2196d0db04b4e9
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-04-11 20:55:24 +0300

    new

---
+++ sfc/content.js
@@ -0,0 +1,10 @@
+const Content =()=> {
+  return (
+    <div className="content">
+    <div className="content-blocks"></div>
+    <div className="content-blocks"></div>
+    </div>
+  );
+}
+
+export default Content;
